8b697f044956c6c2067f3a66fc5b2795a6fa2f7f
Omar El Hajj <omar.elhajj.00@gmail.com>
2023-09-28 20:38:48 +0300

    correct some bugs

--- crm-app/Controllers/CustomerController.cs
+++ crm-app/Controllers/CustomerController.cs
@@ -1,5 +1,6 @@
 ﻿using crm_app.Models;
 using Microsoft.AspNetCore.Mvc; // Import the necessary namespace for MVC controllers.
+using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Tooling.Connector;
 
 namespace crm_app.Controllers
@@ -16,22 +17,37 @@ namespace crm_app.Controllers
         // This is the Create action method
         public IActionResult Create(CustomerModel model) // Define the Create action method that takes a CustomerModel parameter.
         {
+            if (model is null)
+            {
+                throw new ArgumentNullException(nameof(model));
+            }
             // Retrieve Dynamics 365 connection string from app settings
             var connectionString = _configuration.GetConnectionString("Dynamics365"); // Retrieve the connection string named "Dynamics365" from the configuration.
 
             // Create a connection to Dynamics 365
-            using (var service = new CrmServiceClient(connectionString)) // Create a connection to Dynamics 365 using the retrieved connection string.
+            using var service = new CrmServiceClient(connectionString); // Create a connection to Dynamics 365 using the retrieved connection string.
+            if (service.IsReady)
             {
-                // Use the CRM SDK to interact with Dynamics 365 here
-                // e.g., create a new record in Dynamics 365 based on the user input
+                // Create a new CRM entity (e.g., contact)
+                var contact = new Entity("contact");
+                contact["firstname"] = model.Name;
+                contact["emailaddress1"] = model.Email;
+                contact["telephone1"] = model.Phone;
 
-                // The code inside this block is where you would perform operations on Dynamics 365, such as creating records.
+                // Create the record
+                service.Create(contact);
+
+                // Handle success or redirect to an appropriate page
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // Handle connection error
+                return RedirectToAction("Error");
             }
 
-            // Redirect to a success page or handle errors
-            return RedirectToAction("Success"); // Redirect to the "Success" action or another action after completing the operation.
-        }
 
-        // Other action methods for your CustomerController go here
+            // Other action methods for your CustomerController go here
+        }
     }
 }
